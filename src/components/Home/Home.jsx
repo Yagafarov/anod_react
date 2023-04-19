916f53c441b577de81875a213a9eb38a7500bff6
Yagafarov <dinmuhammadyagafarov@gmail.com>
2023-04-19 20:17:57 +0500

    Upload carousel itmems

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,40 +1,72 @@
 import React from 'react';
-import {Carousel, Row, Col, Button} from 'antd';
-import './Home.css'
-
+import {Carousel,Image, Row, Col, Button} from 'antd';
+import imag1 from "../../assets/main1.png";
+import("./Home.css")
 const Home = () => (
   <Carousel autoplay className='home'>
     <div className={'navbarItem'}>
       <Row className="corItem">
-          <Col span={12}>
-              <h1>
-                  Lorem ipsum dolor sit amet.
+          <Col xs={24} md={12}>
+              <h1  className={"headerText"}>
+                  Lorem ipsum dolor sit amet, consectetur.
               </h1>
               <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis commodi corporis culpa doloremque, doloribus iusto pariatur quae, quis similique suscipit.
               </p>
-              <Button type={'default'}>Salom</Button>
+              <Button  type={'default'}>Boshlash</Button>
           </Col>
-          <Col span={12}>22</Col>
+          <Col span={12}>
+              <Image  src={imag1} preview={false}/>
+          </Col>
       </Row>
     </div>
     <div className={'navbarItem'}>
-      <Row className="corItem">
-          <Col span={12}>11</Col>
-          <Col span={12}>22</Col>
-      </Row>
+        <Row className="corItem">
+            <Col span={12}>
+                <h1  className={"headerText"}>
+                    Lorem ipsum dolor sit amet, consectetur.
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis commodi corporis culpa doloremque, doloribus iusto pariatur quae, quis similique suscipit.
+                </p>
+                <Button  type={'default'}>Boshlash</Button>
+            </Col>
+            <Col span={12}>
+                <Image src={imag1} preview={false}/>
+            </Col>
+        </Row>
     </div>
     <div className={'navbarItem'}>
-      <Row className="corItem">
-          <Col span={12}>11</Col>
-          <Col span={12}>22</Col>
-      </Row>
+        <Row className="corItem">
+            <Col span={12}>
+                <h1  className={"headerText"}>
+                    Lorem ipsum dolor sit amet, consectetur.
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis commodi corporis culpa doloremque, doloribus iusto pariatur quae, quis similique suscipit.
+                </p>
+                <Button  type={'default'}>Boshlash</Button>
+            </Col>
+            <Col span={12}>
+                <Image  src={imag1} preview={false}/>
+            </Col>
+        </Row>
     </div>
     <div className={'navbarItem'}>
-      <Row className="corItem">
-          <Col span={12}>11</Col>
-          <Col span={12}>22</Col>
-      </Row>
+        <Row className="corItem">
+            <Col span={12}>
+                <h1  className={"headerText"}>
+                    Lorem ipsum dolor sit amet, consectetur.
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores blanditiis commodi corporis culpa doloremque, doloribus iusto pariatur quae, quis similique suscipit.
+                </p>
+                <Button  type={'default'}>Boshlash</Button>
+            </Col>
+            <Col span={12}>
+                <Image  src={imag1} preview={false}/>
+            </Col>
+        </Row>
     </div>
   </Carousel>
 );
